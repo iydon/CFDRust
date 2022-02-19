e027ee41966c0acc692e2ae3bf66e9556ad085cc
Iydon Liang <liangiydon@gmail.com>
2022-02-19 18:48:15 +0800

    :art: Optimize sample code

---
+++ mae5005/homework/1/bailey_borwein_plouffe_formula.f90
@@ -0,0 +1,104 @@
+! In 1995 (very recent in terms of the history of mathematics), Simon Plouffe discovered an amazing formulae for computing π, which, in principle, allows one to compute π to any binary (decimal) digit. His formulae is
+!
+! \pi = \sum_{k=0}^{\infty} \left[
+!     \frac{1}{16^k} \left(
+!         \frac{4}{8k+1} - \frac{2}{8k+4} - \frac{1}{8k+5} - \frac{1}{8k+6}
+!     \right)
+! \right]
+!
+! Clearly, the different terms in terms of k, decreases quickly as k is increased. Here, we shall use the above formulae to compute π, at different accuracies and investigate how many terms are needed for each case.
+!
+! (a) Write a single precision Fortran code, to compute π using the above formulae;
+! (b) Write a double precision Fortran code, to compute π using the above formulae;
+! (c) Write a quadruple precision Fortran code, to compute π using the above formulae.
+!
+! Report, in each case, what do you obtain? what is the accuracy? How many terms, in terms of k, are needed to achieve the accuracy? Your code for each part is expected to be less than 20 lines. The value of π, up to 100th decimal places, is given here
+!
+! π = 3.1415926535 8979323846 2643383279 5028841971 6939937510
+!       5820974944 5923078164 0628620899 8628034825 3421170679
+
+program main
+   use, intrinsic :: iso_fortran_env, only: f32=>real32, f64=>real64, f128=>real128
+   implicit none
+
+   CHARACTER(LEN=23) :: format = '(A13, I4, A7, f36.34)'
+   integer :: n32, n64, n128
+   real(f32) :: pi32, eps32 = 1.4012984643E-45_f32
+   real(f64) :: pi64, eps64 = 4.9406564584124654E-324_f64
+   real(f128) :: pi128, eps128 = 6.4751751194380251109244389582276465525E-4966_f128
+
+   call calc_pi_f32(eps32, n32, pi32)
+   write(*, format) '32-bit: n = ', n32, ', pi = ', pi32
+
+   call calc_pi_f64(eps64, n64, pi64)
+   write(*, format) '64-bit: n = ', n64, ', pi = ', pi64
+
+   call calc_pi_f128(eps128, n128, pi128)
+   write(*, format) '128-bit: n = ', n128, ', pi = ', pi128
+
+   write(*, '(A)') '                   pi = 3.1415926535897932384626433832795029'
+end program main
+
+subroutine calc_pi_f32(eps, n, pi)
+   use, intrinsic :: iso_fortran_env, only: f32=>real32
+
+   real(f32), intent(in) :: eps
+   integer, intent(out)  :: n
+   real(f32), intent(out) :: pi
+
+   real(f32) :: delta, a, b
+
+   n = 1 ; pi = 0.0 ; a = 1.0 ; b = 0.0
+   do while (.true.)
+      delta = (4.0/(b+1.0) - 2.0/(b+4.0) - 1.0/(b+5.0) - 1.0/(b+6.0)) / a
+      if (abs(delta) < eps) then
+         exit
+      end if
+      n = n + 1 ; pi = pi + delta ; a = a * 16.0 ; b = b + 8.0
+   end do
+end subroutine calc_pi_f32
+
+subroutine calc_pi_f64(eps, n, pi)
+   use, intrinsic :: iso_fortran_env, only: f64=>real64
+
+   real(f64), intent(in) :: eps
+   integer, intent(out)  :: n
+   real(f64), intent(out) :: pi
+
+   real(f64) :: delta, a, b
+
+   n = 1 ; pi = 0.0 ; a = 1.0 ; b = 0.0
+   do while (.true.)
+      delta = (4.0/(b+1.0) - 2.0/(b+4.0) - 1.0/(b+5.0) - 1.0/(b+6.0)) / a
+      if (abs(delta) < eps) then
+         exit
+      end if
+      n = n + 1 ; pi = pi + delta ; a = a * 16.0 ; b = b + 8.0
+   end do
+end subroutine calc_pi_f64
+
+subroutine calc_pi_f128(eps, n, pi)
+   use, intrinsic :: iso_fortran_env, only: f128=>real128
+
+   real(f128), intent(in) :: eps
+   integer, intent(out)  :: n
+   real(f128), intent(out) :: pi
+
+   real(f128) :: delta, a, b
+
+   n = 1 ; pi = 0.0 ; a = 1.0 ; b = 0.0
+   do while (.true.)
+      delta = (4.0/(b+1.0) - 2.0/(b+4.0) - 1.0/(b+5.0) - 1.0/(b+6.0)) / a
+      if (abs(delta) < eps) then
+         exit
+      end if
+      n = n + 1 ; pi = pi + delta ; a = a * 16.0 ; b = b + 8.0
+   end do
+end subroutine calc_pi_f128
+
+! #[test]
+! [
+!     {
+!         "stdout": " 32-bit: n =   33, pi = 3.1415925025939941406250000000000000\n 64-bit: n =  257, pi = 3.1415926535897931159979634685441852\n128-bit: n = 4097, pi = 3.1415926535897932384626433832795024\n                   pi = 3.1415926535897932384626433832795029\n"
+!     }
+! ]
---
+++ mae5005/homework/1/input_and_output.f90
@@ -0,0 +1,61 @@
+! Using the department Unix computer and a simple Fortran code, investigate how an input real number is outputted (i.e., interpreted by a computer in bits and bytes) under the single precision representation.
+!
+! (a) Input = 0.61E−45
+! (b) Input = 0.82E−45
+! (c) Input = 2.13E−45
+! (d) Input = 2.34E−45
+! (e) Input = 3.65E−10
+! (f) Input = 4.06E+30
+! (g) Input = 3.40E+38
+! (h) Input = 3.41E+38
+!
+! Namely, in each case, (i) report the actual output of your input; (ii) explain the precise binary form used by the computer. Your code is expected to be less than 10 lines.
+!
+! Hint: you may consult the Wiki page at https://en.wikipedia.org/wiki/Single-precision_floating-point_format
+
+program main
+   use, intrinsic :: iso_fortran_env, only: sp=>real32
+   implicit none
+
+   real(sp) :: number
+
+   write(*, fmt='(A)', advance='no') 'Input  = '
+   read(*, *) number
+   write(*, fmt='(A, ES14.8, A, b32.32)') 'Output = ', number, ' = ', transfer(number, 0)
+end program main
+
+! #[test]
+! [
+!     {
+!         "stdin": "0.61E-45",
+!         "stdout": "Input  = Output = 0.00000000E+00 = 00000000000000000000000000000000\n"
+!     },
+!     {
+!         "stdin": "0.82E-45",
+!         "stdout": "Input  = Output = 1.40129846E-45 = 00000000000000000000000000000001\n"
+!     },
+!     {
+!         "stdin": "2.13E-45",
+!         "stdout": "Input  = Output = 2.80259693E-45 = 00000000000000000000000000000010\n"
+!     },
+!     {
+!         "stdin": "2.34E-45",
+!         "stdout": "Input  = Output = 2.80259693E-45 = 00000000000000000000000000000010\n"
+!     },
+!     {
+!         "stdin": "3.65E-10",
+!         "stdout": "Input  = Output = 3.65000002E-10 = 00101111110010001010100100101111\n"
+!     },
+!     {
+!         "stdin": "4.06E+30",
+!         "stdout": "Input  = Output = 4.05999996E+30 = 01110010010011001111101001000101\n"
+!     },
+!     {
+!         "stdin": "3.40E+38",
+!         "stdout": "Input  = Output = 3.39999995E+38 = 01111111011111111100100110011110\n"
+!     },
+!     {
+!         "stdin": "3.41E+38",
+!         "stdout": "Input  = Output =       Infinity = 01111111100000000000000000000000\n"
+!     }
+! ]
